ac883d4bda0ac45002bd60f5213443cae1404332
billowstao <22577367+billowstao@users.noreply.github.com>
2023-01-21 15:14:44 +0800

    style(prettier): 添加 prettier
    - 添加 `prettier.config.cjs` 配置文件
    - 添加 `.prettierignore` 配置文件

---
+++ prettier.config.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+  semi: true,
+  singleQuote: true,
+  quoteProps: 'as-needed',
+  trailingComma: 'es5',
+  bracketSpacing: true,
+  htmlWhitespaceSensitivity: 'css',
+  endOfLine: 'lf',
+  embeddedLanguageFormatting: 'auto',
+};
